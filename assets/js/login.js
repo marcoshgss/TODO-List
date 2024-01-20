597191ac51931759a80503830e285b04f119c5b1
Marcos Henrique <sena_marcos.fp@hotmail.com>
2024-01-20 13:54:03 -0300

    add-autenticação-login

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,9 +7,31 @@ const btn = document.querySelector(".fa-eye");
 btn.addEventListener('click', () => {
     const showPassword = document.querySelector("#senha");
 
-    if(showPassword.getAttribute("type") == "password") {
+    if (showPassword.getAttribute("type") == "password") {
         showPassword.setAttribute("type", "text")
     } else {
         showPassword.setAttribute("type", "password")
     }
 })
+
+function entrar() {
+    const usuario = document.querySelector("#usuario");
+    const userLabel = document.querySelector("#userLabel");
+
+    const senha = document.querySelector("#senha");
+    const senhaLabel = document.querySelector("#senhaLabel");
+
+    const mensagemErro = document.querySelector("#mensagemErro");
+    let listaUser = []
+
+    let userValid = {
+        nome: '',
+        user: '',
+        senha: ''
+    }
+
+    listaUser = JSON.parse(localStorage.getItem('listaUser'))
+
+    console.log(listaUser)
+}
+
