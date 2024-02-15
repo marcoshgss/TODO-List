5d389b9f74eedef76a43b77e649cbe28b9ca7dbe
Marcos Henrique <sena_marcos.fp@hotmail.com>
2024-02-15 18:58:59 -0300

    add-botão-sair-e-hambúrguer

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,18 @@ const filterBtn = document.querySelector("#filter-select");
 let oldInputValue;
 
 // Funções
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if  (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src="assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src="assets/img/close_white_36dp.svg";
+    }
+}
+
 let userLogado = JSON.parse(localStorage.getItem('userLogado'))
 
 let logado = document.querySelector('#logado');
